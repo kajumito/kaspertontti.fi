607b937f2c38f572090f576927ca9e4ea2b51703
kajumito <kasper@gnonce.com>
2019-05-21 20:26:48 +0800

    google fonts

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -3,12 +3,12 @@ import Typography from 'typography'
 const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: 1.25,
-  // googleFonts: [
-  //   {
-  //     name: 'Montserrat',
-  //     styles: ['400', '500', '700'],
-  //   },
-  // ],
+  googleFonts: [
+    {
+      name: 'Montserrat',
+      styles: ['400', '500', '700'],
+    },
+  ],
   headerFontFamily: ['Montserrat', 'Arial', 'sans-serif'],
   bodyFontFamily: ['Montserrat', 'Arial', 'sans-serif'],
 })
